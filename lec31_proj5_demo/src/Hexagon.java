9fb1c7bae66c527a0f97e5872c30239b45f42df6
Josh Hug <hug@cs.berkeley.edu>
2025-11-10 20:39:57 -0800

    add live code for swe II lecture

--- lec31_proj5_demo/src/Hexagon.java
+++ lec31_proj5_demo/src/Hexagon.java
@@ -1,4 +1,5 @@
 import tileengine.TETile;
+import tileengine.Tileset;
 
 public class Hexagon {
     // side length s
@@ -7,11 +8,33 @@ public class Hexagon {
     int topLeftY;
     int size;
     TETile biome;
-    public Hexagon(int topLeftX, int topLefY, int s, TETile t) {
+    public Hexagon(int topLeftX, int topLeftY, int s, TETile t) {
         this.topLeftX = topLeftX;
         this.topLeftY = topLeftY;
         size = s;
         biome = t;
+    }
+
+    // could be pure, could be not
+    // by pure I mean it has not side effects, and returns a copy
+    // we picked IMPURE
+    public void addHexagon(TETile[][] world) {
+        // top half
+        for (int rowNum = 0; rowNum < size; rowNum += 1) {
+            Lec31.drawHorizontalLine(world, topLeftX - rowNum, topLeftY - rowNum, size + 2 * rowNum, biome);
+        }
+
+        // bottom half
+        // first call drawHorizontalLine(x - size, y - size, 2 * size)
+        // second call drawHorizontalLine(x - size + 1, y - size + 1, 2 * size - 2)
+        for (int rowNum = 0; rowNum < size; rowNum += 1) {
+            int firstX = topLeftX - size + 1;
+            int firstY = topLeftY - size;
+            int firstLength = 3 * size - 2;
+            Lec31.drawHorizontalLine(world, firstX + rowNum, firstY - rowNum, firstLength - 2 * rowNum, biome);
+        }
+
+        //        drawHorizontalLine(world, 10, 10, 13, Tileset.LOCKED_DOOR);
     }
 
     //public Hexagon(int topLeftX, int topLefY, int s, TETile t, List<Hexagon> neighbors);
--- lec31_proj5_demo/src/Lec31.java
+++ lec31_proj5_demo/src/Lec31.java
@@ -1,33 +1,103 @@
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
 import tileengine.TERenderer;
 import tileengine.TETile;
 import tileengine.Tileset;
+
+import java.util.Random;
 
 /**
  * Draws a hexagon world (or at least that's the goal).
  */
 public class Lec31 {
-    public static void main(String[] args) {
-        TERenderer ter = new TERenderer();
-        ter.initialize(40, 40);
+    private static int WIDTH = 45;
+    private static int HEIGHT = 45;
+    private static int HEX_SIZE = 3;
 
-        TETile[][] world = new TETile[40][40];
-        for (int x = 0; x < 40; x += 1) {
-            for (int y = 0; y < 40; y += 1) {
+    public static void main(String[] args) {
+
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
                 world[x][y] = Tileset.NOTHING;
             }
         }
 
-        Hexagon demoHexagon = new Hexagon(10, 10, 4, Tileset.TREE);
 
+        int xStart = 10;
+        int yStart = 30;
+        drawHexColumn(world, xStart, yStart, HEX_SIZE, 3);
+        xStart += HEX_SIZE * 2 - 1;
+        yStart += HEX_SIZE;
+        drawHexColumn(world, xStart, yStart, HEX_SIZE, 4);
+        xStart += HEX_SIZE * 2 - 1;
+        yStart += HEX_SIZE;
+        drawHexColumn(world, xStart, yStart, HEX_SIZE, 5);
+        xStart += HEX_SIZE * 2 - 1;
+        yStart -= HEX_SIZE;
+        drawHexColumn(world, xStart, yStart, HEX_SIZE, 4);
+        xStart += HEX_SIZE * 2 - 1;
+        yStart -= HEX_SIZE;
+        drawHexColumn(world, xStart, yStart, HEX_SIZE, 3);
 
-        drawHorizontalLine(world, 10, 10, 13, Tileset.LOCKED_DOOR);
         ter.renderFrame(world);
+
+        int playerX = 25;
+        int playerY = 25;
+        TETile theStuffWhereIStanding = world[playerX][playerY];
+
+        while (true) {
+            if (StdDraw.hasNextKeyTyped()) {
+                char c = StdDraw.nextKeyTyped();
+                world[playerX][playerY] = theStuffWhereIStanding;
+                if (c ==  'w') {
+                    playerY += 1;
+                }
+                if (c == 'a') {
+                    playerX -= 1;
+                }
+                if (c == 's') {
+                    playerY -= 1;
+                }
+                if (c == 'd') {
+                    playerX += 1;
+                }
+
+                theStuffWhereIStanding = world[playerX][playerY];
+                world[playerX][playerY] = Tileset.AVATAR;
+                ter.renderFrame(world);
+            }
+        }
+    }
+
+    // Draw a column of hexagons of hte appropriate size
+    // starting from x, y as the top left of the first such hexagon
+    public static void drawHexColumn(TETile[][] world, int x, int y, int size, int numHexagons) {
+
+        for (int i = 0; i < numHexagons; i += 1) {
+
+            int biomeNumber = StdRandom.uniformInt(5);
+            TETile t = null;
+            switch (biomeNumber) {
+                case 0: t = Tileset.MOUNTAIN; break;
+                case 1: t = Tileset.SAND; break;
+                case 2: t = Tileset.GRASS; break;
+                case 3: t = Tileset.FLOWER; break;
+                case 4: t = Tileset.TREE; break;
+                default: break;
+            }
+
+            Hexagon tempHex = new Hexagon(x, y - 2*size*i, size, t);
+            tempHex.addHexagon(world);
+        }
     }
 
     public static void drawHorizontalLine(TETile[][] world, int x, int y, int size, TETile t) {
         for (int xOffset = 0; xOffset < size; xOffset += 1) {
-            world[x + xOffset][y] = t;
+            world[x + xOffset][y] = TETile.colorVariant(t, 32, 32, 32, new Random());
         }
     }
-
 }
