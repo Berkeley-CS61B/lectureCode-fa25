96aa03c90d848f1de8a141c68430a9a8234db4b6
Henry Hutchings <flarkensam@gmail.com>
2025-12-03 16:55:21 -0800

    Just a diamond because I'm lazy

--- lec40_git/Diamond.java
+++ lec40_git/Diamond.java
@@ -1,11 +1,9 @@
 import java.awt.Color;
 
-// This acts as the "default" shape so the screen isn't empty.
 public class Diamond implements PartyShape {
-
     public void draw(Turtle t) {
         t.setColor(Color.GREEN);
-        // Draw a square using primitive turtle moves
+        // Draw a diamond using primitive turtle moves
         for (int i = 0; i < 2; i++) {
             t.forward(60);
             t.turnRight(110);
